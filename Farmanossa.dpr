d337dc3a52746f32151caa811a0f2b89227a15dc
leocristian <leonardosclopes@gmail.com>
2022-05-30 16:11:40 -0300

    salvando mainfile

--- Farmanossa.dpr
+++ Farmanossa.dpr
@@ -29,7 +29,8 @@ uses
   u_relatorios in 'units\relatorios\u_relatorios.pas' {PagRelatorios},
   u_filtro_rel_1 in 'units\relatorios\filtros\u_filtro_rel_1.pas' {FormRel1},
   u_filtro_rel_2 in 'units\relatorios\filtros\u_filtro_rel_2.pas' {FormRel2},
-  u_filtro_rel_3 in 'units\relatorios\filtros\u_filtro_rel_3.pas' {FormRel3};
+  u_filtro_rel_3 in 'units\relatorios\filtros\u_filtro_rel_3.pas' {FormRel3},
+  u_filtro_rel_4 in 'units\relatorios\filtros\u_filtro_rel_4.pas' {FormRel4};
 
 {$R *.res}
 
@@ -61,6 +62,7 @@ begin
   Application.CreateForm(TFormRel1, FormRel1);
   Application.CreateForm(TFormRel2, FormRel2);
   Application.CreateForm(TFormRel3, FormRel3);
+  Application.CreateForm(TFormRel4, FormRel4);
   Application.Run;
 end.
 
